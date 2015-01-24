0162660ca0c32c14e64c008862bbeb08f3c2feaa
DullReferenceException <jpage@godaddy.com>
2015-01-23 20:47:17 -0800

    Test for the path placeholder

---
+++ test/specs/placeholders/path.tests.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var test = require('../../util');
+
+describe('The path placeholder', function() {
+  var app;
+
+  before(function(done) {
+    test.startHTTPApp(function(err, result) {
+      app = result;
+      done(err);
+    });
+  });
+
+  it('outputs the URL without the query string', function(done) {
+    var rule = { from: /.*/, to: 'http://newdomain.com{path}' };
+    var opts = { url: test.baseUrl + 'some/path?param=value' };
+    app.verifyRules(rule, opts, function(err, res) {
+      expect(err).to.not.exist;
+      expect(res.statusCode).to.equal(301);
+      expect(res.headers.location).to.equal('http://newdomain.com/some/path');
+      done();
+    });
+  });
+
+  after(function(done) {
+    app.stop(done);
+  });
+});
